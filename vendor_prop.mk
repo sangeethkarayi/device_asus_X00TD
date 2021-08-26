6c47cd6bc469e5cb450130621b5bf5e2024ab8fb
Dyneteve <sound0020@gmail.com>
2021-08-26 20:00:00 +0530

    X00TD: Enable AOSP surfaceflinger
    * AOSP SurfaceFlinger is smoother than CAF
    
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -133,6 +133,21 @@ PRODUCT_PRODUCT_PROPERTIES += \
     dalvik.vm.dex2oat-threads=4 \
     dalvik.vm.image-dex2oat-cpu-set=4,5,6,7
 
+# Display (SF)
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.early_phase_offset_ns=1500000 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.support_kernel_idle_timer=true \
+    ro.surface_flinger.use_frame_rate_api=true \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.protected_contents=true
+
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
